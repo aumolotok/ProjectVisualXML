4fc4bfa453e2fced717095471c62f8e82c35ee81
aumolotok <aumolotok@mail.ru>
2017-05-21 01:36:43 +0300

    Test Commit
    Test Commit

--- MappingTool/MappingTool/MappingTool/Form1.cs
+++ MappingTool/MappingTool/MappingTool/Form1.cs
@@ -16,5 +16,11 @@ namespace MappingTool
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            i = 5;
+        }
     }
 }
